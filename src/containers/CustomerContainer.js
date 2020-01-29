3c24712e22caeb093debce60d931f889f6488947
Erwin Bustillo <erwinb1996@gmail.com>
2020-01-29 17:44:13 -0500

    big changes

--- src/containers/CustomerContainer.js
+++ src/containers/CustomerContainer.js
@@ -1,19 +1,51 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import {Route} from 'react-router-dom'
+import {Route, withRouter} from 'react-router-dom'
 import { connect } from 'react-redux'
 import {getCustomerByDni} from '../selectors/customers'
 import AppFrame from '../components/AppFrame'
 import CustomerEdit from '../components/CustomerEdit'
 import CustomerData from '../components/CustomerData'
+import {fetchCustomers} from '../actions/fetchCustomers'
+import {updateCustomer} from '../actions/updateCustomer'
 export class CustomerContainer extends Component {
 
+    componentDidMount() {
+        if(!this.props.customer)
+            this.props.fetchCustomers();
+    }
+    
+    
+    handleSubmit = values => {
+        console.log(JSON.stringify(values));
+        const {id} = values;
+        return this.props.updateCustomer(id,values)
+    }
+
+    handleOnBack = ()=>{
+        this.props.history.goBack();
+    }
+
+    handleOnSubmitSuccess = ()=>{
+        this.handleOnBack();
+    }
 
     renderBody = ()=> (
         <Route path="/customers/:dni/edit" children={
             ({ match })=> {
-                const CustomerControl = match ?  CustomerEdit : CustomerData; 
-                return <CustomerControl {...this.props.customer}/>
+
+                if(this.props.customer){
+                    const CustomerControl = match ?  CustomerEdit : CustomerData; 
+                    return <CustomerControl 
+                        onSubmit={this.handleSubmit}
+                        onSubmitSuccess={this.handleOnSubmitSuccess}
+                        onBack={this.handleOnBack} 
+                        {...this.props.customer}/>
+                }
+
+                return null
+
+                
             } 
         } />
     )
@@ -34,10 +66,15 @@ export class CustomerContainer extends Component {
 CustomerContainer.propTypes = {
     dni: PropTypes.string.isRequired,
     customer: PropTypes.object,
+    fetchCustomers: PropTypes.func.isRequired,
+    updateCustomer: PropTypes.func.isRequired,
 }
 
 const mapStateToProps = (state,props) => ({
     customer: getCustomerByDni(state,props)
 })
 
-export default connect(mapStateToProps, null)(CustomerContainer)
+export default withRouter(connect(mapStateToProps, {
+    fetchCustomers,
+    updateCustomer
+})(CustomerContainer)) 
